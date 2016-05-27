932cc48226184e133ab91ac1573eccbf1b39639e
IARyan <ryan.grandgenett@gmail.com>
2016-05-27 16:25:55 -0500

    Stub GUI

--- utils.py
+++ utils.py
@@ -1,55 +1,112 @@
-#!/usr/bin/python -tt
-
-"""
-LG Optimus Exceed 2 VS450PP (w5c) bootloader unlock tool
-"""
-
 import sys
 import os
+from subprocess import check_output
 
 def in_path(program):
   """
   Checks if a program is in the PATH.
   """
   fpath, fname = os.path.split(program)
+  print fpath
+  print fname
   if fpath:
+    print "FPATh!!!!!"
+    print program
     if os.path.isfile(program) and os.access(program, os.X_OK):
+      print "print found %s from path!" % (program)
       return True
   else:
     for path in os.environ["PATH"].split(os.pathsep):
       path = path.strip('"')
       exe_file = os.path.join(path, program)
       if os.path.isfile(exe_file) and os.access(exe_file, os.X_OK):
+        print "MMMMMMMMMAAAAAAAAAAAAADDDDDDDDDDDEEEEEEEEEEEEEEEE IIIIIIIIIIIIIITTTTTTTTTTTTTTT!!!!!!!"
+        print exe_file
         return True
 
     return False
 
-def check_dependencies(dependencies):
+def check_dependencies(sdk_path, gui=False):
   """
   Verifies dependencies are installed.
   """
+  dependencies = []
+
+  # Check if sdk_path is specified
+  if sdk_path:
+    dependencies.append(os.path.join(sdk_path,'platform-tools', 'adb'))
+    dependencies.append(os.path.join(sdk_path,'platform-tools', 'fastboot'))
+  else:
+    dependencies.append('adb')
+    dependencies.append('fastboot')
+
+  # Make sure dependencies are installed
   for dependency in dependencies:
-    print "Checking [-] %s ..." % (dependency)
-    if not is_installed(dependency):
-      print "[-] %s was not found in the path is it installed?" % (dependency)
-      sys.exit(1)
+    if not in_path(dependency):
+      message = '%s was not found in the PATH. To fix download the Android SDK and add the platform-tools folder to the PATH\n Alternatively you can pass the path to the SDK in on the command line (-s or --sdk) ' % (dependency)
+      if gui:
+        return {'status': 'Error', 'message': message}
+      else:
+          print message 
+          sys.exit(1)
 
 def fastboot(command):
   """
   Runs provided fastboot command.
   """
-  for dependency in dependencies:
-    print "Checking [-] %s ..." % (dependency)
-    if not is_installed(dependency):
-      print "[-] %s was not found in the path is it installed?" % (dependency)
-      sys.exit(1)
+  try:
+   return check_output([command])
+  except subprocess.CalledProcessError:
+    print e.output
+    sys.exit(1)
 
 def adb(command):
   """
   Runs provided adb command.
   """
-  for dependency in dependencies:
-    print "Checking [-] %s ..." % (dependency)
-    if not is_installed(dependency):
-      print "[-] %s was not found in the path is it installed?" % (dependency)
-      sys.exit(1)
+  try:
+   return check_output([command])
+  except subprocess.CalledProcessError:
+    print e.output
+    sys.exit(1)
+
+def unlock_bootloader():
+  """
+  Unlocks the LG Optimus Exceed 2 bootloader.
+  """
+  try:
+   print "[+] Unlocking bootloader" 
+  except subprocess.CalledProcessError:
+    print e.output
+    sys.exit(1)
+
+
+def lock_bootloader():
+  """
+  Runs provided adb command.
+  """
+  try:
+   return check_output([command])
+  except subprocess.CalledProcessError:
+    print e.output
+    sys.exit(1)
+
+def fastboot_mode():
+  """
+  Runs provided adb command.
+  """
+  try:
+   return check_output([command])
+  except subprocess.CalledProcessError:
+    print e.output
+    sys.exit(1)
+
+def download_mode():
+  """
+  Runs provided adb command.
+  """
+  try:
+   return check_output([command])
+  except subprocess.CalledProcessError:
+    print e.output
+    sys.exit(1)
--- w5c_unlock.py
+++ w5c_unlock.py
@@ -3,100 +3,44 @@
 """
 LG Optimus Exceed 2 VS450PP (w5c) bootloader unlock tool
 """
-
 import argparse
 import utils
-import sys
-import os
-from subprocess import check_output
+import Tkinter
 
-def in_path(program):
+class unlock_gui(Tkinter.Tk):
   """
-  Checks if a program is in the PATH.
+  GUI code executed when no command line parameters were provided. 
   """
-  fpath, fname = os.path.split(program)
-  if fpath:
-    if os.path.isfile(program) and os.access(program, os.X_OK):
-      return True
-  else:
-    for path in os.environ["PATH"].split(os.pathsep):
-      path = path.strip('"')
-      exe_file = os.path.join(path, program)
-      if os.path.isfile(exe_file) and os.access(exe_file, os.X_OK):
-        return True
+  def __init__(self,parent):
+    Tkinter.Tk.__init__(self,parent)
+    self.parent = parent
+    self.initialize()
 
-    return False
+  def initialize(self):
+    self.grid()
 
-def check_dependencies(dependencies):
-  """
-  Verifies dependencies are installed.
-  """
-  for dependency in dependencies:
-    print "Checking [-] %s ..." % (dependency)
-    if not in_path(dependency):
-      print "[-] %s was not found in the path is it installed?" % (dependency)
-      sys.exit(1)
+    gui_actions = Tkinter.LabelFrame(self, text="Unlocker Actions")
+    gui_actions.grid(row=0, column=0, columnspan=4, sticky='NSEW', padx=5, pady=5, ipadx=5, ipady=5)
 
-def fastboot(command):
-  """
-  Runs provided fastboot command.
-  """
-  try:
-   return check_output([command])
-  except subprocess.CalledProcessError:
-    print e.output
-    sys.exit(1)
+    unlock_button = Tkinter.Button(gui_actions,text=u"Unlock Bootloader", command = utils.fastboot)
+    unlock_button.grid(row=1, column=0, columnspan=2, sticky='W')
 
-def adb(command):
-  """
-  Runs provided adb command.
-  """
-  try:
-   return check_output([command])
-  except subprocess.CalledProcessError:
-    print e.output
-    sys.exit(1)
+    lock_button = Tkinter.Button(gui_actions,text=u"Lock Bootloader", command = utils.fastboot)
+    lock_button.grid(row=1, column=2, columnspan=2, sticky='W')
 
-def unlock_bootloader():
-  """
-  Unlocks the LG Optimus Exceed 2 bootloader.
-  """
-  try:
-   print "[+] Unlocking bootloader" 
-  except subprocess.CalledProcessError:
-    print e.output
-    sys.exit(1)
+    unlock_button = Tkinter.Button(gui_actions,text=u"Fastboot Mode", command = utils.fastboot )
+    unlock_button.grid(row=2, column=0, columnspan=2, sticky='W')
+
+    unlock_button = Tkinter.Button(gui_actions,text=u"Download Mode", command = utils.fastboot)
+    unlock_button.grid(row=2, column=2, columnspan=2, sticky='W')
 
 
-def lock_bootloader():
-  """
-  Runs provided adb command.
-  """
-  try:
-   return check_output([command])
-  except subprocess.CalledProcessError:
-    print e.output
-    sys.exit(1)
+    output_frame = Tkinter.LabelFrame(self, text="Unlocker Output")
+    output_frame.grid(row=3, column=0, columnspan=4, sticky='W', padx=5, pady=5, ipadx=5, ipady=5)
 
-def fastboot_mode():
-  """
-  Runs provided adb command.
-  """
-  try:
-   return check_output([command])
-  except subprocess.CalledProcessError:
-    print e.output
-    sys.exit(1)
 
-def download_mode():
-  """
-  Runs provided adb command.
-  """
-  try:
-   return check_output([command])
-  except subprocess.CalledProcessError:
-    print e.output
-    sys.exit(1)
+    output_window = Tkinter.Text(output_frame)
+    output_window.grid(row=4, column=0, columnspan=4)
 
 
 def main():
@@ -107,36 +51,25 @@ def main():
     parser.add_argument('-l', '--lock',action='store_true', default=False, dest="lock_bootloader", help="Lock Optimus Exceed 2 bootloader")
     parser.add_argument('-f', '--fastboot-mode', action='store_true', default=False, dest="fastboot_mode", help="Set Optimus Exceed 2 to fastboot mode")
     parser.add_argument('-d', '--download-mode', action='store_true', default=False, dest="download_mode", help="Set Optimus Exceed 2 to download mode")
+    parser.add_argument('-s', '--sdk', action='store', dest="sdk_path", help="Path to Android SDK (optional)")
 
     # Parse the command line arguments.
     args = parser.parse_args()
-    if not vars(args):
-      parser.print_help()
-      parser.exit(1)
 
-    # Verify an IP argument was provided.
-    if not args.ip:
-      parser.print_help()
-      sys.exit(0)
-
-    # Verify a command line argument was provided.
-    if not args.unlock and not args.lock and not args.fastboot and not args.download:
-      print "[-] Error you must specifiy an action i.e., (unlock, lock, fastboot-mode, download-mode)"
-      sys.exit(1)
-
-    # Make sure adb and fastboot are installed
-    print "[+] Checking that dependencies are installed"
-    check_dependencies(['adb', 'fastboot'])
-
-    # Handle command line argument 
+    # Handle command line arguments if provided else start the GUI
     if args.unlock_bootloader:
-      unlock_bootloader()
+      utils.check_dependencies(args.sdk_path, gui=False)
+      #unlock_bootloader(, )
     elif args.lock_bootloader:
-      lock_bootloader()
+      utils.lock_bootloader()
     elif args.fastboot_mode:
-      fastboot_mode()
+      utils.fastboot_mode()
     elif args.download_mode:
-      download_mode()
+      utils.download_mode()
+    else:
+      app = unlock_gui(None)
+      app.title('LG Optimus Exceed 2 Unlocker')
+      app.mainloop()
   except KeyboardInterrupt:
     sys.exit(1)
 
