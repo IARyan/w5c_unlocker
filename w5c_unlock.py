426e38b37d6d908d1be8301b87d21503b41f39db
IARyan <ryan.grandgenett@gmail.com>
2016-05-28 13:44:11 -0500

    GUI fixups

--- w5c_unlock.py
+++ w5c_unlock.py
@@ -14,6 +14,10 @@ class unlock_gui(tk.Tk):
   """
   def __init__(self,parent):
     tk.Tk.__init__(self,parent)
+    self.output_window = None
+    self.adb_status = None
+    self.fastboot_status = None
+    self.file_status = None
     self.parent = parent
     self.resizable(0,0)
     self.grid()
@@ -36,19 +40,14 @@ class unlock_gui(tk.Tk):
     # Allow grid auto resize 
     gui_actions.grid_rowconfigure(0, weight=1)
     gui_actions.grid_columnconfigure(0, weight=1)
-
     gui_actions.grid_rowconfigure(0, weight=1)
     gui_actions.grid_columnconfigure(1, weight=1)
-
     gui_actions.grid_rowconfigure(1, weight=1)
     gui_actions.grid_columnconfigure(0, weight=1)
-
     gui_actions.grid_rowconfigure(1, weight=1)
     gui_actions.grid_columnconfigure(1, weight=1)
-
     gui_actions.grid_rowconfigure(2, weight=1)
     gui_actions.grid_columnconfigure(0, weight=1)
-
     gui_actions.grid_rowconfigure(2, weight=1)
     gui_actions.grid_columnconfigure(1, weight=1)
 
@@ -70,45 +69,52 @@ class unlock_gui(tk.Tk):
     # Create UnRoot button
     tk.Button(gui_actions, text=u"Un-Root Device", width=20, height=2, command = utils.fastboot).grid(row=2, column=1, padx=10, pady=10)
 
-    self.grid_rowconfigure(1, weight=1)
-    self.grid_columnconfigure(0, weight=1)
 
   def status(self):
-   # Actions label frame
-    tool_status = tk.LabelFrame(self, text="Dependency Status")
+    # Actions label frame
+    tool_status = tk.LabelFrame(self, text="Dependencies Status")
     tool_status.grid(row=0, column=1, padx=5, pady=5, sticky="NSEW")
+
+    # Allow grid auto resize 
+    tool_status.grid_rowconfigure(0, weight=1)
+    tool_status.grid_columnconfigure(1, weight=1)
+    tool_status.grid_rowconfigure(1, weight=1)
+    tool_status.grid_columnconfigure(1, weight=1)
+    tool_status.grid_rowconfigure(2, weight=1)
+    tool_status.grid_columnconfigure(1, weight=1)
 
     # Create Unlock button
     tk.Label(tool_status, text="Adb:").grid(row=0, column=0, sticky="E")
-    adb_status = tk.Frame(tool_status, bg="red")
-    tool_status.grid_rowconfigure(0, weight=1)
-    tool_status.grid_columnconfigure(1, weight=1)
-    adb_status.grid(row=0, column=1, sticky="NSEW", padx=5, pady=10)
+    self.adb_status = tk.Frame(tool_status, bg="#EE0000")
 
+    self.adb_status.grid(row=0, column=1, sticky="NSEW", padx=5, pady=10)
+    
     tk.Label(tool_status, text="Fastboot:").grid(row=1, column=0, sticky="E")
-    adb_status = tk.Frame(tool_status, bg="red")
-    tool_status.grid_rowconfigure(1, weight=1)
-    tool_status.grid_columnconfigure(1, weight=1)
-    adb_status.grid(row=1, column=1, sticky="NSEW", padx=5, pady=10)
+    self.fastboot_status = tk.Frame(tool_status, bg="#EE0000")
 
-    tk.Label(tool_status, text="Files:").grid(row=2, column=0, sticky="E")
-    adb_status = tk.Frame(tool_status, bg="red")
-    tool_status.grid_rowconfigure(2, weight=1)
-    tool_status.grid_columnconfigure(1, weight=1)
-    adb_status.grid(row=2, column=1, sticky="NSEW", padx=5, pady=10)
-    #tk.Button(tool_status, text=u"Unlock Bootloader", width=20, height=2, command = utils.fastboot).grid(row=0, column=0, sticky="NSEW", padx=10, pady=10)
+    self.fastboot_status.grid(row=1, column=1, sticky="NSEW", padx=5, pady=10)
+
+    tk.Label(tool_status, text="Install Files:").grid(row=2, column=0, sticky="E")
+    self.file_status = tk.Frame(tool_status, bg="#EE0000")
+  
+    self.file_status.grid(row=2, column=1, sticky="NSEW", padx=5, pady=10)
+    self.file_status.configure(background="#009900")
 
   def output(self):
+    # Allow grid to auto resize 
+    self.grid_rowconfigure(1, weight=1)
+    self.grid_columnconfigure(0, weight=1)
+
+    # Output label frame
     output_frame = tk.LabelFrame(self, text="Unlocker Output")
     output_frame.grid(row=1, column=0, columnspan=2, padx=5, pady=5, ipadx=8, ipady=8, sticky="NSEW")
 
 
-    output_window = gui_utils.ScrolledTextReadOnly(output_frame, height=10)
-    output_window.insert(tk.INSERT, "A" * 1000 + "\n")
+    self.output_window = gui_utils.ScrolledTextReadOnly(output_frame, height=10)
+    self.output_window .insert(tk.INSERT, "A" * 1000 + "\n")
     output_frame.grid_rowconfigure(0, weight=1)
     output_frame.grid_columnconfigure(0, weight=1)
-    output_window.grid(row=0, column=0)
-
+    self.output_window .grid(row=0, column=0)
 
 def main():
   try:
